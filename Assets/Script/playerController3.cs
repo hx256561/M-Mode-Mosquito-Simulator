33098f50973f943f95f6bd806f0d3127cab981cb
Lil Ang <45808441+hx256561@users.noreply.github.com>
2021-12-28 21:31:33 +0800

    防止蚊子穿牆

---
+++ Assets/Script/playerController3.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController3 : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 move;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Jump");
+
+        if (Input.GetAxis("Jump") != 0)
+        {
+            move = transform.right * x +transform.up*y+ transform.forward * z;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+        else
+        {
+            move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+        }
+        /*
+        move = transform.right * x + transform.forward * z;
+
+        controller.Move(move*speed*Time.deltaTime);*/
+    }
+}

fb495e0285dc0c9495c990e2bc66c409fa449e78
Lil Ang <45808441+hx256561@users.noreply.github.com>
2022-01-06 15:27:47 +0800

    新增和雞的對話

--- Assets/Script/chicken.cs
+++ Assets/Script/chicken.cs
@@ -6,8 +6,12 @@ public class chicken : MonoBehaviour
 {
 
     public GameObject panel0;
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
     public Transform player;
     private bool pShow = true;
+    private int story = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -27,11 +31,39 @@ public class chicken : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return) && dist.magnitude <= 5)
         {
             pShow = false;
+
+            switch (story)
+            {
+                case 0:
+                    panel0.SetActive(true);
+                    break;
+                case 1:
+                    panel0.SetActive(false);
+                    panel1.SetActive(true);
+                    break;
+                case 2:
+                    panel1.SetActive(false);
+                    panel2.SetActive(true);
+                    break;
+                case 3:
+                    panel2.SetActive(false);
+                    panel3.SetActive(true);
+                    break;
+            }
         }
 
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            story += 1;
+        }
+
+        
+
+        /*
         if (dist.magnitude > 5)
         {
             pShow = true;
         }
+        */
     }
 }
